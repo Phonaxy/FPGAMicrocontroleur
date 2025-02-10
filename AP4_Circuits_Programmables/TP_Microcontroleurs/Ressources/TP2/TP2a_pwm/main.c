e8cc39dc224123584b4b631b0a6a1179807db635
RomainD <44780490+Phonaxy@users.noreply.github.com>
2025-02-10 15:20:57 +0100

    Update pwm

--- AP4_Circuits_Programmables/TP_Microcontroleurs/Ressources/TP2/TP2a_pwm/main.c
+++ AP4_Circuits_Programmables/TP_Microcontroleurs/Ressources/TP2/TP2a_pwm/main.c
@@ -1,25 +1,54 @@
-#include <xc.h>
+/* 
+ * File: main.c 
+ * Author: Destruhaut Romain
+ * Comments: JUNIA AP4 2024-2025
+ * Revision history: 
+    * 2025/02/10 : File created
+ */
 
-void main(void) {
-    // Initialize Ports
-    TRISBbits.TRISB0 = 1;   // RB0 comme input (Le bouton S1)
-    TRISDbits.TRISD0 = 0;   // RD0 comme output (la LED D1)
-    
-    ANSELBbits.ANSB0 = 0;   // Set de RB0 comme output digital
-    ANSELD = 0x00;          // Set de PORTD comme outputs digital
-    
-    LATDbits.LATD0 = 0;     // LED D1 disable au démarrage
+/* Résultat : La LED est allumée à 10% au début, et quand on appuie sur S1, elle passe à 100% grâce au PWM */
 
-    while (1) {
-        // Check de l'appui du bouton avec la logique active low
-        if (PORTBbits.RB0 == 0) {  
-            if (PORTBbits.RB0 == 0) {  // Confirmation que le bouton est encore préssé
-                LATDbits.LATD0 = 1;    // Allumage de la led D1
-                while (PORTBbits.RB0 == 0); // Tant que le bouton n'est pas relâché
-            }
-        } else {
-            LATDbits.LATD0 = 0;        // Extinction led D1 sur relâchement
-        }
-    }
+#include "configbits.h"
+#include <xc.h>      
+
+#define _XTAL_FREQ 8000000
+
+// Fonction qui initialise le Timer 2
+void init_timer2(void) {
+    T2CON=0x06; // Configuration de base
+    // Le Timer 2 sert à générer un délai (pour le PWM)
+    T2CONbits.T2CKPS = 0;  // Prescaler
+    T2CONbits.T2OUTPS = 7;   // Postcaler
+    PR2 = 199;  // Valeur de PR2
+    TMR2 = 0;   // Remise à zéro du compteur
+    T2CONbits.TMR2ON = 1;  // On démarre le Timer 2
+    PIR1bits.TMR2IF = 0;  // On reset le flag d'interruption
+    PIE1bits.TMR2IE = 1;  // On active l'interruption (mais on l'utilise pas ?)
 }
 
+void main(void) {
+    // Configuration des ports
+    TRISD = 0x00; // Port D en sortie (pour la LED)
+    LATD = 0x00;  // On met tout à 0 au début
+    
+    TRISBbits.TRISB0 = 1;   // RB0 en entrée (c'est le bouton S1)
+    ANSELBbits.ANSB0 = 0;   // On met RB0 en digital
+    
+    // Configuration du PWM
+    PWM4EN=0x1;    // Active le PWM
+    PWM4DCL=0x00;  // PWM duty cycle bas mis à 0
+    RD0PPS=0b01111; // On assigne la sortie PWM au pin RD0
+    
+
+    // Initialisation du Timer 2
+    init_timer2();
+    
+    while (1) {
+         // Vérification si le bouton est appuyé (active low = appui -> 0)
+        if (PORTBbits.RB0 == 0) {  
+            PWM4DCH=0xC7; // Met la LED à 100%
+        } else {
+            PWM4DCH=0x14; // Met la LED à 10%
+        }         
+    }
+}
